cc6cfeb15bdfc2e7366ed293d6fa1c2c05778d8b
dranyam2022 <maynardescalante@gmail.com>
2022-11-06 15:45:25 +0800

    expense-tracker

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import ExpenseForm from "./ExpenseForm";
+import './NewExpense.css'
+
+
+export default function NewExpense(props) {
+
+    const [isEditing, setIsEditing] = useState(false)
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.floor(Math.random() * Math.random() * 123456789).toString()
+        }
+        props.onAddExpense(expenseData)
+        setIsEditing(false)
+    }
+
+    const startEditingHandler = () => {
+        setIsEditing(true)
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false)
+    }
+
+
+    return (
+        <div className='new-expense'>
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} />}
+        </div>
+    )
+}
